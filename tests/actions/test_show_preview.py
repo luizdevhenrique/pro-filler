b0005c13e272378a3c7648a52d3750db189a0f3a
luizdevhenrique <luiz.henrique.ribeiro018@gmail.com>
2024-06-06 16:05:36 -0300

    requisito 3 feito

--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -1,1 +1,25 @@
 from pro_filer.actions.main_actions import show_preview  # NOQA
+
+
+def test_show_preview_context(capsys):
+    context = {"all_files": [], "all_dirs": []}
+
+    show_preview(context)
+    result = capsys.readouterr()
+    assert result.out == "Found 0 files and 0 directories\n"
+
+
+def test_show_preview(capsys):
+    context = {
+        "all_files": ["src/__init__.py",
+                      "src/app.py", "src/utils/__init__.py"],
+        "all_dirs": ["src", "src/utils"]
+    }
+    show_preview(context)
+    result = capsys.readouterr()
+    assert result.out == (
+        "Found 3 files and 2 directories\n"
+        "First 5 files: ['src/__init__.py',"
+        " 'src/app.py', 'src/utils/__init__.py']\n"
+        "First 5 directories: ['src', 'src/utils']\n"
+    )
