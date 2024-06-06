86c947a9f9c4a38eeae2e6a587259600ae5135e5
luizdevhenrique <luiz.henrique.ribeiro018@gmail.com>
2024-06-06 17:05:50 -0300

    agora sim, requisito 3 feito

--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -27,15 +27,14 @@ def test_show_preview(capsys):
 
 def test_show_preview_does_not_slice(capsys):
     context = {
-        "all_files": ["src/__init__.py",
-                      "src/app.py", "src/utils/__init__.py"],
-        "all_dirs": ["src", "src/utils"]
+        "all_files": ["file" + str(i) for i in range(10)],
+        "all_dirs": ["dir" + str(i) for i in range(10)]
     }
     show_preview(context)
     result = capsys.readouterr()
     assert result.out == (
-        f"Found {len(context['all_files'])}"
-        f" files and {len(context['all_dirs'])} directories\n"
-        f"All files: {context['all_files']}\n"
-        f"All directories: {context['all_dirs']}\n"
+        f"Found {len(context['all_files'])} files and"
+        f" {len(context['all_dirs'])} directories\n"
+        f"First 5 files: {context['all_files'][:5]}\n"
+        f"First 5 directories: {context['all_dirs'][:5]}\n"
     )
