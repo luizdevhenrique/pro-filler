467945d0740de8233bc9add0e4bbd9dedfc1d4de
luizdevhenrique <luiz.henrique.ribeiro018@gmail.com>
2024-06-06 16:35:46 -0300

    mudanças

--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -19,6 +19,23 @@ def test_show_preview(capsys):
     result = capsys.readouterr()
     assert result.out == (
         "Found 3 files and 2 directories\n"
-        "Files: ['src/__init__.py', 'src/app.py', 'src/utils/__init__.py']\n"
-        "Directories: ['src', 'src/utils']\n"
+        "First 5 files: ['src/__init__.py', 'src/app.py',"
+        " 'src/utils/__init__.py']\n"
+        "First 5 directories: ['src', 'src/utils']\n"
+    )
+
+
+def test_show_preview_does_not_slice(capsys):
+    context = {
+        "all_files": ["src/__init__.py",
+                      "src/app.py", "src/utils/__init__.py"],
+        "all_dirs": ["src", "src/utils"]
+    }
+    show_preview(context)
+    result = capsys.readouterr()
+    assert result.out == (
+        f"Found {len(context['all_files'])}"
+        f" files and {len(context['all_dirs'])} directories\n"
+        f"All files: {context['all_files']}\n"
+        f"All directories: {context['all_dirs']}\n"
     )
