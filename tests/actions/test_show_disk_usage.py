d36e6341d2859b8e6f1c246f7134a44bf5337e36
luizdevhenrique <luiz.henrique.ribeiro018@gmail.com>
2024-06-07 18:47:13 -0300

    requisito 5 feito

--- tests/actions/test_show_disk_usage.py
+++ tests/actions/test_show_disk_usage.py
@@ -1,1 +1,27 @@
 from pro_filer.actions.main_actions import show_disk_usage  # NOQA
+import pytest
+
+
+@pytest.fixture
+def create_files(tmp_path):
+    files = ["file1.txt", "file2.txt", "file3.txt"]
+    for file in files:
+        path = tmp_path / file
+        path.write_text("This is some content")
+
+    return [str(tmp_path / file) for file in files]
+
+
+def test_show_disk_usage(create_files, capsys):
+    context = {
+        "all_files": create_files
+    }
+    show_disk_usage(context)
+    result = capsys.readouterr()
+    lines = result.out.split("\n")
+
+    assert len(lines) == 5
+    assert "Total size: 51" in lines[-2]
+
+    for line in lines[:-2]:
+        assert "(33.33%)" in line
