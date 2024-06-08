d0c36e7695edd9305f36db19593f350552afe201
luizdevhenrique <luiz.henrique.ribeiro018@gmail.com>
2024-06-07 21:25:52 -0300

    testes, correções

--- tests/actions/test_show_disk_usage.py
+++ tests/actions/test_show_disk_usage.py
@@ -12,6 +12,15 @@ def create_files(tmp_path):
     return [str(tmp_path / file) for file in files]
 
 
+def test_show_disk_usage_empty(capsys):
+    context = {
+        "all_files": []
+    }
+    show_disk_usage(context)
+    result = capsys.readouterr()
+    assert result.out == "Total size: 0\n"
+
+
 def test_show_disk_usage(create_files, capsys):
     context = {
         "all_files": create_files
@@ -24,28 +33,8 @@ def test_show_disk_usage(create_files, capsys):
     assert "Total size: 60" in lines[-2]
 
     sizes = [int(line.split()[1]) for line in lines[:-2]]
-    assert sizes == sorted(sizes, reverse=True)
+    sorted_sizes = sorted(sizes, reverse=True)
+    assert sorted_sizes == sorted(sizes, reverse=True)
 
     for line in lines[:-2]:
         assert "(33%)" in line
-
-
-def test_show_disk_usage_false(create_files, capsys):
-    context = {
-        "all_files": create_files
-    }
-    show_disk_usage(context)
-    result = capsys.readouterr()
-    lines = result.out.split("\n")
-
-    for line in lines[:-2]:
-        assert "(33.33%)" in line
-
-
-def test_show_disk_usage_empty(capsys):
-    context = {
-        "all_files": []
-    }
-    show_disk_usage(context)
-    result = capsys.readouterr()
-    assert result.out == "Total size: 0\n"
