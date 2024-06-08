e370ff97137bba137d2309a9d199efd407c1b062
luizdevhenrique <luiz.henrique.ribeiro018@gmail.com>
2024-06-07 22:03:32 -0300

    alterações

--- tests/actions/test_find_duplicate_files.py
+++ tests/actions/test_find_duplicate_files.py
@@ -5,7 +5,7 @@ from pro_filer.actions.main_actions import find_duplicate_files
 def test_find_duplicate_files_no_duplicates(tmp_path):
     (tmp_path / "file1.txt").write_text("Hello, World!")
     (tmp_path / "file2.txt").write_text("Hello, Python!")
-    (tmp_path / "file3.txt").write_text("Hello, GitHub Copilot!")
+    (tmp_path / "file3.txt").write_text("Hello, GitHub!")
 
     context = {
         "all_files": [
